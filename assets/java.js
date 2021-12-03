f858524146bfe5262e23147ccdbec1210fdf49bf
Chris <chrisramirez15@yahoo.com>
2021-12-03 03:30:18 -0600

    added questions in javascript

---
+++ assets/java.js
@@ -0,0 +1,45 @@
+//Questions for quiz
+
+const quizData = [
+    {
+        question: "Commonly used data types DO not include...",
+        a: "Strings",
+        b: "Booleans",
+        c: "Alerts",
+        d: "Numbers",
+        correct: "c",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _______",
+        a: "Quotes",
+        b: "Curly brackets",
+        c: "Parenthesis",
+        d: "Square brackets",
+        correct: "b",
+    },
+    {
+        question: "Arrays in javascript can be used to store ______",
+        a: "NUmbers & Strings",
+        b: "Other arrays",
+        c: "Booleans",
+        d: "all of the above",
+        correct: "d",
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        a: "Commas",
+        b: "Curly brackets",
+        c: "Quotes",
+        d: "Parenthesis",
+        correct: "c",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        a: "Javascript",
+        b: "Terminal/Bash",
+        c: "For Loops",
+        d: "Console.log",
+        correct: "d",
+    },
+];
+
